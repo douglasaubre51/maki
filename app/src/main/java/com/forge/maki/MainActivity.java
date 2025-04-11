92dd7ea53e53f0ce2715d01871b7c78f4a2c0cae
douglasaubre51 <boseallen192@gmail.com>
2025-04-11 14:54:32 +0530

    create my first app

--- app/src/main/java/com/forge/maki/MainActivity.java
+++ app/src/main/java/com/forge/maki/MainActivity.java
@@ -1,13 +1,33 @@
 package com.forge.maki;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity  {
-    @Override
-    protected void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
+import android.widget.Toast;
+import android.widget.Button;
+import android.widget.EditText;
 
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //load layout file in res
         setContentView(R.layout.activity_main);
+        // logic
+        Button submitBtn = findViewById(R.id.submitBtn);
+        EditText nameBox = findViewById(R.id.nameBox);
+
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // on btn click logic
+                String text = "welcome to android world user - " + nameBox.getText().toString();
+
+                Toast.makeText(MainActivity.this, text, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
